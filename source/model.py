c35939a9dcf77587bad61c7fde99a5f97019165c
jhsansom <jhsansom@gmail.com>
2023-01-09 20:14:40 -0500

    Adding race data pipeline

---
+++ source/model.py
@@ -0,0 +1,21 @@
+
+
+class Model:
+
+    def __init__(self):
+        pass
+
+    def train(self, training_data):
+        # minibatch data
+
+        # call predict on each minibatch
+
+        # use cross entropy as loss function
+
+        # use gradient descent to update the model
+
+        pass
+
+
+    def test(self):
+        pass
